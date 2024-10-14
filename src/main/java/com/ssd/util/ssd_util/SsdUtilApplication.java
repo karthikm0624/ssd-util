c74f5489bf1e7a4545c9dcb7cf3df1bb93fdbcc2
karthikm0624 <karthik.margam93@gmail.com>
2024-10-14 12:02:57 +0530

    ssd-util push-

---
+++ src/main/java/com/ssd/util/ssd_util/SsdUtilApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.util.ssd_util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdUtilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssd/util/ssd_util/SsdUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.util.ssd_util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
